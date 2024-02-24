8daf4cb8de6b4cd245163f1b1c6b8476426f82cf
Saurabh Wankhade <drsaurabh2018@gmail.com>
2024-02-24 16:54:04 +0530

    Img src imporved

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ const userRole = document.querySelector('.user_role');
 const userAbout = document.querySelector('.user_about');
 
 const data = [
-    {'imgSrc' : 'media/user1.jpg',
+    {'imgSrc' : 'Media/user1.jpg',
     'userName' : 'Tom Buluchka',
     'userRole':'Designer','userAbout' : `Design Info didn’t begin its ventures with the name of Design Info. The timeline goes back to 1969 when Saremal began his first venture in Mumbai as a supplier of books at the age of 17. The genre was mainly textiles & fashion magazines.`},
 
 
-    {'imgSrc' : 'media/user2.jpg',
+    {'imgSrc' : 'Media/user2.jpg',
     'userName' : 'Tim Brim','userRole':'Web-Developer','userAbout' : `Pricinples of honesty and hardwork with numerous travel tripos accross the country imporved the presence of the company. Textile forecast books and fashion color forecast books were major additions in the 80s. `},
 
 
-    {'imgSrc' : 'media/user3.jpg',
+    {'imgSrc' : 'Media/user3.jpg',
     'userName' : ' Johnny Depp','userRole':'Software Engineer','userAbout' : `Brand name “Design Info” was launched in 2008 when BSA decided to venture into photography as an addition to the existing color management business and cater to all professionals in this segment.  `},
 ];
 
